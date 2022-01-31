c26a24b38c2b326f9e8957a520c30923af02af61
victormln <victor.molinaf@gmail.com>
2022-01-31 01:18:21 +0100

    Adding form_handler.php

--- public/index.php
+++ public/index.php
@@ -26,7 +26,7 @@
         <div class="row align-items-md-stretch">
             <div class="col-md-12">
                 <div class="h-100 p-5 text-white bg-dark rounded-3">
-                    <form action="../src/ZoomChatConverterController.php" method="post" enctype="multipart/form-data">
+                    <form action="form_handler.php" method="post" enctype="multipart/form-data">
                         <h2>Instructions</h2>
                         <ol>
                             <li>Upload your zoom chat. (<a href="https://support.zoom.us/hc/en-us/articles/115004792763-Guardar-el-chat-en-la-reuni%C3%B3n" target="_blank" class="link-info"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16"><path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/></svg> More info about how to get it</a>)</li>
